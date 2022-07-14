091897ab21edad7a56e34def055429d3fda4fdcb
rick.liu <rick.liu@dgevolt.com>
2022-07-14 14:17:18 +0800

    Creat a simple .net project with console

---
+++ MyApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+Console.WriteLine("Verify .Net command: dotnet");
+Console.WriteLine("Creat new App: dotnet new console -o MyApp -f net6.0 ");
+Console.WriteLine("Or use: dotnet new console -o MyApp ");
+Console.WriteLine("Go to the dirty: cd MyApp ");
+Console.WriteLine("Run program: dotnet run ");
+
+Console.WriteLine("---- 其他运行命令 ---- ");
+Console.WriteLine("保存代码后自动运行: dotnet watch run ");
+Console.WriteLine("最简易的创建 .Net 工程: dotnet new console ");
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # LearnCsharp
+
+## Document
+
+- 学习 C# 笔记
+
+参考网址：
+
+- http://c.biancheng.net/csharp/
+
+## HelloWorld
+
+学习 C# 中的例程，源代码，使用 Visual Studio 2019 开发
+
+## MyApp
+
+使用控制台（console），创建的 C# 编程代码
